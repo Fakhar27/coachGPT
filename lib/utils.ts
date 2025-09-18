c3544650e50604601294dfbf24a03163fa996ecc
Fakhar27 <fakharmursaleen12345@gmail.com>
2025-09-18 23:58:28 +0500

    lib chnages

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,8 +8,4 @@ export function cn(...inputs: ClassValue[]) {
 // This check can be removed, it is just for tutorial purposes
 export const hasEnvVars =
   process.env.NEXT_PUBLIC_SUPABASE_URL &&
-<<<<<<< HEAD
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY;
-=======
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
->>>>>>> 100052ec116f38a4a6bb0bc64c2055a352fdbe26
